83b7c3f621913d5f4c0bcb53a8592528ce0547ff
Rushabh <rushabhbarot812@gmail.com>
2024-10-31 00:38:50 +0530

    added test cases for author controller integration testing

--- src/main/java/com/SpringBoot/TestingHW/advises/GlobalExceptionHandler.java
+++ src/main/java/com/SpringBoot/TestingHW/advises/GlobalExceptionHandler.java
@@ -3,8 +3,12 @@ package com.SpringBoot.TestingHW.advises;
 import com.SpringBoot.TestingHW.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -18,6 +22,23 @@ public class GlobalExceptionHandler {
         return buildErrorResponseEntity(apiError);
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<?>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+
+        List<String> errors = ex.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(objectError -> objectError.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        ApiError apiError = ApiError.builder()
+                .message(ex.getLocalizedMessage())
+                .subErrors(errors)
+                .httpStatus(HttpStatus.BAD_REQUEST)
+                .build();
+
+        return buildErrorResponseEntity(apiError);
+    }
 
     private ResponseEntity<ApiResponse<?>> buildErrorResponseEntity(ApiError apiError) {
         return new ResponseEntity<>(new ApiResponse<>(apiError),apiError.getHttpStatus());
--- src/test/java/com/SpringBoot/TestingHW/controller/AuthorControllerTestIT.java
+++ src/test/java/com/SpringBoot/TestingHW/controller/AuthorControllerTestIT.java
@@ -1,5 +1,6 @@
 package com.SpringBoot.TestingHW.controller;
 
+import com.SpringBoot.TestingHW.dto.AuthorDTO;
 import com.SpringBoot.TestingHW.entity.AuthorEntity;
 import org.junit.jupiter.api.Test;
 
@@ -12,5 +13,144 @@ class AuthorControllerTestIT extends AbstractIntegrationTest{
         //we saved the author to get it afterward
         AuthorEntity savedAuthor = authorRepository.save(mockAuthorEntity);
 
+        webTestClient.get()
+                .uri("/author")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.data.[0].id").isEqualTo(savedAuthor.getId())
+                .jsonPath("$.data.[0].name").isEqualTo(savedAuthor.getName());
+    }
+
+    @Test
+    void testGetAuthorById_success() {
+        AuthorEntity savedAuthor = authorRepository.save(mockAuthorEntity);
+
+        webTestClient.get()
+                .uri("/author/{authorId}",savedAuthor.getId())
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.data.id").isEqualTo(savedAuthor.getId())
+                .jsonPath("$.data.name").isEqualTo(savedAuthor.getName());
+    }
+
+    @Test
+    void testGetAuthorById_failure() {
+        webTestClient.get()
+                .uri("/author/999")
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
+    void testCreateNewAuthor_whenValidAuthor_thenCreate(){
+        webTestClient.post()
+                .uri("/author")
+                .bodyValue(mockAuthorDto)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody()
+                .jsonPath("$.data.name").isEqualTo(mockAuthorDto.getName());
+    }
+
+    @Test
+    void testCreateNewAuthor_whenInvalidAuthor_thenReturnBadRequest(){
+        //create an invalid author here just 2 characters whereas there should be 3 minimum
+        AuthorDTO authorDTO = AuthorDTO.builder()
+                .name("Jo")
+                .build();
+
+        webTestClient.post()
+                .uri("/author")
+                .bodyValue(authorDTO)
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody()
+                .jsonPath("$.apiError.subErrors[0]").isEqualTo("Name of the author must be at least 3 characters");
+    }
+
+    @Test
+    void testUpdateAuthorById_whenValidDto_thenReturnUpdatedAuthor() {
+        //save dummy author
+        AuthorEntity savedAuthor = authorRepository.save(mockAuthorEntity);
+
+        //now update it
+        AuthorDTO updatedAuthorDto = AuthorDTO.builder()
+                .id(savedAuthor.getId())
+                .name("UPDATED NAME")
+                .build();
+
+        webTestClient.put()
+                .uri("/author/{authorId}", savedAuthor.getId())
+                .bodyValue(updatedAuthorDto)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.data.name").isEqualTo(updatedAuthorDto.getName());
+    }
+
+    @Test
+    void testUpdateAuthorById_whenInvalidDto_thenReturnBadRequest() {
+        AuthorEntity savedAuthor = authorRepository.save(mockAuthorEntity);
+
+        AuthorDTO invalidAuthorDto = AuthorDTO.builder()
+                .id(savedAuthor.getId())
+                .name("JO") // Invalid name
+                .build();
+
+        webTestClient.put()
+                .uri("/author/{authorId}", savedAuthor.getId())
+                .bodyValue(invalidAuthorDto)
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody()
+                .jsonPath("$.apiError.subErrors[0]").isEqualTo("Name of the author must be at least 3 characters");
+    }
+
+    @Test
+    void testDeleteAuthorById_success() {
+        AuthorEntity savedAuthor = authorRepository.save(mockAuthorEntity);
+
+        webTestClient.delete()
+                .uri("/author/{authorId}", savedAuthor.getId())
+                .exchange()
+                .expectStatus().isNoContent();
+
+        webTestClient.get()
+                .uri("/author/{authorId}", savedAuthor.getId())
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
+    void testDeleteAuthorById_failure() {
+        webTestClient.delete()
+                .uri("/authors/999")
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
+    void testGetAuthorsByName_success() {
+        AuthorEntity savedAuthor = authorRepository.save(mockAuthorEntity);
+
+        webTestClient.get()
+                .uri("/author/name/{name}", savedAuthor.getName())
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.data.[0].id").isEqualTo(savedAuthor.getId())
+                .jsonPath("$.data.[0].name").isEqualTo(savedAuthor.getName());
+    }
+
+    @Test
+    void testGetAuthorsByName_emptyResult() {
+        webTestClient.get()
+                .uri("/author/name/NonExistentName")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.data").isEmpty();
     }
 }
